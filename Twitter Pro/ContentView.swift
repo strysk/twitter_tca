e76c05c5a9769adf35688f5aedba7b6895740a89
Ryosuke Seto <setoryosuke0306@gmail.com>
2021-09-24 00:16:33 +0900

    Initial Commit

---
+++ Twitter Pro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Twitter Pro
+//
+//  Created by Ryosuke Seto on 2021/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Twitter Pro/Twitter_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_ProApp.swift
+//  Twitter Pro
+//
+//  Created by Ryosuke Seto on 2021/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
